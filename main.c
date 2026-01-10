0a89e888ba161368f132f947f949f5da777b0218
Mateusz Szarkowski <romek.dmow@gmail.com>
2026-01-10 16:11:21 +0100

    wypisanie planu MENU

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h> 
 int main() {
-    printf("Hello World!\n");
+    printf("Hello World!43125123\n");
     return 0;
 }
+ // tutaj bedzie intruskcja MAIN z całym MENU programu(niedokonczona)
+ // int main() {
+ // int wybor;
+ // do {
+ //    printf("\n-----MENU------\n");
+ //    printf("1. numer ID wpisu\n");
+ //    printf("2. nazwa Autora\n");
+ //    printf("3. Treść posta\n");
+ //    printf("4. Kategoria Zgłoszenia\n");
+ //    printf("5. Liczba Zgłoszeń\n");
+ //    printf("6. Status Moderacji\n");
+ //    printf("\n 0. -----KONIEC PROGRAMU--------\n");
+ //    scanf("%d", &wybor);
+ //    switch(wybor) {
+ //          case 1:
+ //                 tu bedzie cos
+ //                 break;
+ //           case 2:
+ //                 tu bedzie cos
+ //                 break;
+ //           case 3:
+ //                 tu bedzie cos
+ //                 break;
+ //          case 4:
+ //                 tu bedzie cos
+ //                 break;
+ //          case 5:
+ //                 tu bedzie cos
+ //                 break;
+ //          case 6:
+ //                 tu bedzie cos
+ //                 break;
+ //           case 0:
+ //                 tu bedzie cos
+ //                 break;
+ //           default:
+ //                 tu bedzie cos     
+ //       }
+ //   } while (wybor != 0);
+ //   return 0;
+ // }
