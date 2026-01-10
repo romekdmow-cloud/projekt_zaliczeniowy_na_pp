2d2ea43bcdbf9b6efe8f725934e36d2ee2483a18
Mateusz Szarkowski <romek.dmow@gmail.com>
2026-01-10 17:04:55 +0100

    definicja struktury post i enuma status

--- main.c
+++ main.c
@@ -1,10 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h> 
+#include <string.h>
+
+#define MAX_AUTOR 100
+#define MAX_TRESC 280
+#define MAX_KATEGORIA 30
+
+typedef enum {
+    DO_WERYFIKACJI, 
+    W_TRAKCIE,
+    ZATWIERDZONE, 
+    USUNIETE
+} StatusPosta;
+
+typedef struct Post {
+    int id;
+    char autor[MAX_AUTOR];
+    char tresc[MAX_TRESC];
+    char kategoria[MAX_KATEGORIA];
+    int liczba_zgloszen;
+    StatusPosta status;
+    struct Post* nastepny;
+} Post;
+
+const char* statusDoTekstu(StatusPosta s) {
+    switch (s) {
+        case DO_WERYFIKACJI: return "Do weryfikacji";
+        case W_TRAKCIE: return "W trakcie analizy";
+        case ZATWIERDZONE: return "Zatwierdzone";
+        case USUNIETE: return "Usuniete";
+        deafult: return "Nieznany";
+    }
+}
 int main() {
-    printf("Hello World!43125123\n");
+    printf("System Moderacji QuickTalk - Inicjalizacja struktur...\n");
+    Post testowyPost;
+    testowyPost.id = 1;
+
+    strcpy(testowyPost.autor, "Incognito_ziomuś007");
+    strcpy(testowyPost.tresc, "To jest przykładowy post o polityce");
+    strcpy(testowyPost.kategoria, "szydera");
+    testowyPost.liczba_zgloszen = 0;
+    testowyPost.status = ZATWIERDZONE;
+    testowyPost.nastepny = NULL; 
+
+    printf("ID: %d | Autor: %s | Status: %s\n", 
+           testowyPost.id, 
+           testowyPost.autor, 
+           statusDoTekstu(testowyPost.status));
     return 0;
 }
- // tutaj bedzie intruskcja MAIN z całym MENU programu(niedokonczona)
+ // tutaj miała być intruskcja MAIN z całym MENU programu(niedokonczona)
  // int main() {
  // int wybor;
  // do {
