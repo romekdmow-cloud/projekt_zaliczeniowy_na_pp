2edfccb27daafeb81ff933ba42330859ef7fde9d
Mateusz Szarkowski <romek.dmow@gmail.com>
2026-01-11 17:39:02 +0100

    wdrażanie funkcji stworzPost i obsługa pamięci malloc/free

--- main.c
+++ main.c
@@ -32,62 +32,40 @@ const char* statusDoTekstu(StatusPosta s) {
         deafult: return "Nieznany";
     }
 }
+
+Post* stworzPost(int id, const char* autor, const char* tresc) {
+    Post* nowy = (Post*)malloc(sizeof(Post));
+    if (nowy == NULL) {
+        printf("Błąd, brak pamieci!\n");
+        return NULL;
+    }
+
+nowy->id = id;
+strcpy(nowy->autor, autor);
+strcpy(nowy->tresc, tresc);
+strcpy(nowy->kategoria, "ogolna"); 
+nowy->liczba_zgloszen = 0;
+nowy->status = DO_WERYFIKACJI;
+nowy->nastepny = NULL;
+
+return nowy;
+}
+
 int main() {
-    printf("System Moderacji QuickTalk - Inicjalizacja struktur...\n");
-    Post testowyPost;
-    testowyPost.id = 1;
+    printf("System Moderacji QuickTalk\n");
+    Post* p1 = stworzPost(1, "IncognitoZiomuś007", "alee słoodki kootek!");
+    Post* p2 = stworzPost(2, "Hejter", "nienawidze cie grubasie");
+    
+    if (p1 != NULL) {
+        printf("\nPost ID: %d\nAutor: %s\nTresc: %s\nStatus: %s\n", p1->id, p1->autor, p1->tresc, statusDoTekstu(p1->status));    
+    }
 
-    strcpy(testowyPost.autor, "Incognito_ziomuś007");
-    strcpy(testowyPost.tresc, "To jest przykładowy post o polityce");
-    strcpy(testowyPost.kategoria, "szydera");
-    testowyPost.liczba_zgloszen = 0;
-    testowyPost.status = ZATWIERDZONE;
-    testowyPost.nastepny = NULL; 
-
-    printf("ID: %d | Autor: %s | Status: %s\n", 
-           testowyPost.id, 
-           testowyPost.autor, 
-           statusDoTekstu(testowyPost.status));
+    if (p1 != NULL) {
+        printf("\nPost ID: %d\nAutor: %s\nTresc; %s\nStatus: %s\n", p2->id, p2->autor, p2->tresc, statusDoTekstu(p2->status));
+    }
+    free(p1);
+    free(p2);
+    printf("\nPamiec wyczyszczona --- Koniec Programu. \n");
+    
     return 0;
 }
- // tutaj miała być intruskcja MAIN z całym MENU programu(niedokonczona)
- // int main() {
- // int wybor;
- // do {
- //    printf("\n-----MENU------\n");
- //    printf("1. numer ID wpisu\n");
- //    printf("2. nazwa Autora\n");
- //    printf("3. Treść posta\n");
- //    printf("4. Kategoria Zgłoszenia\n");
- //    printf("5. Liczba Zgłoszeń\n");
- //    printf("6. Status Moderacji\n");
- //    printf("\n 0. -----KONIEC PROGRAMU--------\n");
- //    scanf("%d", &wybor);
- //    switch(wybor) {
- //          case 1:
- //                 tu bedzie cos
- //                 break;
- //           case 2:
- //                 tu bedzie cos
- //                 break;
- //           case 3:
- //                 tu bedzie cos
- //                 break;
- //          case 4:
- //                 tu bedzie cos
- //                 break;
- //          case 5:
- //                 tu bedzie cos
- //                 break;
- //          case 6:
- //                 tu bedzie cos
- //                 break;
- //           case 0:
- //                 tu bedzie cos
- //                 break;
- //           default:
- //                 tu bedzie cos     
- //       }
- //   } while (wybor != 0);
- //   return 0;
- // }
